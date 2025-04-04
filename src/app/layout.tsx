b0d55ff1df5a5b28cc2d20528d119f55890d1a27
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-04-05 00:55:03 +0100

    fix(layout): update metadata property openGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,6 +75,26 @@ export default function RootLayout({
       <head>
       <title>{String(isEnglish ? metadataEn.title : metadataPt.title)}</title>
       <meta name="description" content={String(isEnglish ? metadataEn.description : metadataPt.description)} />
+
+      <meta property="og:title" content={String(isEnglish ? metadataEn.openGraph?.title || "" : metadataPt.openGraph?.title || "")} />
+      <meta property="og:description" content={String(isEnglish ? metadataEn.openGraph?.description || "" : metadataPt.openGraph?.description || "")} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={String(isEnglish ? metadataEn.openGraph?.url || "" : metadataPt.openGraph?.url || "")} />
+      <meta property="og:site_name" content={String(isEnglish ? metadataEn.openGraph?.siteName || "" : metadataPt.openGraph?.siteName || "")} />
+      <meta
+        property="og:image"
+        content={String(
+          isEnglish
+            ? Array.isArray(metadataEn.openGraph?.images)
+              ? (metadataEn.openGraph?.images[0] as { url: string })?.url || ""
+              : (metadataEn.openGraph?.images as { url: string })?.url || ""
+            : Array.isArray(metadataPt.openGraph?.images)
+              ? (metadataPt.openGraph?.images[0] as { url: string })?.url || ""
+              : (metadataPt.openGraph?.images as { url: string })?.url || ""
+        )}
+      />
+      <meta property="og:locale" content={String(isEnglish ? metadataEn.openGraph?.locale || "" : metadataPt.openGraph?.locale || "")} />
+
         <link rel="alternate" href="https://www.brunaalvesphoto.com/" hrefLang="en" />
         <link rel="alternate" href="https://www.brunaalvesphoto.com/pt/" hrefLang="pt" />
         <link rel="alternate" href="https://www.brunaalvesphoto.com/" hrefLang="x-default" />
