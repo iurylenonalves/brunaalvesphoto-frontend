bc78e75cb02681e2898aff177a79cc6960a4fc53
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-09-18 12:21:12 +0100

    feat: integrate vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Questrial, Raleway } from "next/font/google";
 import "./globals.css";
 import type { Metadata } from "next";
@@ -112,6 +113,7 @@ export default function RootLayout({
         </AuthProvider>
         <AosInit />
         <Analytics />
+        <SpeedInsights /> 
       </body>
     </html>
   );
